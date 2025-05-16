2d27a1c43995e3e630c6ecb1f45434e78ccda34b
Danius <danhtinh240521@gmail.com>
2025-05-16 15:08:31 +0700

    Add files via upload

---
+++ Nhanvien.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop_GiuaKi
+{
+   public class NhanVien
+    {
+        public string MaSo { get; set; }
+        public string HoTen { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public string email { get; set; }
+    }
+    
+}
